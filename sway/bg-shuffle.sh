c198e88372652496ae1184b9fe9436f85d1673a9
lcarsos <lcarsos@lcarsos.com>
2022-07-31 23:03:02 -0600

    updating the sway config a bunch

---
+++ sway/bg-shuffle.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# stolen from:
+# https://www.reddit.com/r/swaywm/comments/dvko6o/script_to_discover_active_displays_and_set_a/
+#
+# random_wallpaper
+#
+# Desc: This discovers the names of the active displays and sets
+# a separate random wallpaper for each.
+#
+# Requirements
+# swaywm and swaybg to set wallpaper
+#
+
+# Get outputs/displays from swaymsg
+DISPLAYS=$(swaymsg --type get_outputs --raw | jq --raw-output '.[] |select(.active) | .name')
+
+# Define directory for wallpapers and cache directory for current wallpapers
+WALLPAPER_DIR="$HOME/m/i/startrek"
+CACHE_DIR="$HOME/.cache/sway"
+
+
+killall swaybg # Kill swaybg before assigning wallpaper
+
+# Cycle through outputs/displays
+count=1
+for OUTPUT in $DISPLAYS
+do
+    WALLPAPER=$CACHE_DIR/wallpaper$count.jpg # Set cached current wallpaper name
+
+    # link random wallpaper to cached directory
+    ln -fs $WALLPAPER_DIR/$(ls $WALLPAPER_DIR | shuf -n 1) $WALLPAPER 
+
+    # assign cached wallpaper to correct display
+    swaymsg exec "swaybg -o $OUTPUT -i $WALLPAPER -m fill"
+    count=$((count + 1))
+done
--- sway/status.sh
+++ sway/status.sh
@@ -2,9 +2,25 @@
 
 source ~/.zsh/functions/stardate
 
+japaneseDayOfWeek() {
+    LANG=ja_JP.UTF-8 date +'%a'
+}
+
+calcBattery() {
+    local bat_state=$(upower-i /org/freedesktop/UPower/devices/battery_BAT0 | rg 'state' | awk '{ print $2; }')
+    local percentage=$(upower-i /org/freedesktop/UPower/devices/battery_BAT0 | rg 'percentage' | awk '{ print $2; }')
+    if [[ ${bat_state} == charging ]]; then
+        echo ${percentage}
+    else
+        local timer=$(upower-i /org/freedesktop/UPower/devices/battery_BAT0 | rg 'time to empty' | awk '{ print $4 " " $5; }')
+        echo "${timer-nan} (${percentage})"
+    fi
+}
+
 volume=$(ponymix is-muted && { echo "--" } || { ponymix get-volume })
-battery=$(upower -i /org/freedesktop/UPower/devices/battery_BAT0 | rg percentage | awk '{ print $2; }')
+battery=${calcBattery}
 stardate=$(stardate)
 timestamp=$(date +'%Y-%m-%d %H:%M')
+dayOfWeek=$(japaneseDayOfWeek)
 
-echo "${volume} | ${battery} | $stardate | $timestamp"
+echo "${volume} | ${battery} | $stardate | (${dayOfWeek}) $timestamp"
