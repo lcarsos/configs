fc23d8955ab365d8ef40ab00f73f2f7d1275fd21
echo <echo@lcarsos.com>
2023-07-29 15:13:32 -0600

    modularizing sway config for multiple machines

--- setup.sh
+++ setup.sh
@@ -2,6 +2,7 @@
 
 # A setup script for new machines to uniformly setup new machines.
 
+HOSTNAME=$(cat /etc/hostname)
 configdir=~/c
 
 mkdir -p ~/.config
@@ -12,3 +13,23 @@ ln -s "$configdir/tmux.conf" ~/.tmux.conf
 
 ln -s "$configdir/zsh" ~/.zsh
 ln -s "~/.zsh/zshrc" ~/.zshrc
+
+### SWAY ##################
+
+make_sway_symlink() {
+    local scriptname=$1
+    #local machinespecific=${2:no}
+    ln -s "$configdir/sway/$scriptname" $SWAY_DIR/$scriptname
+}
+
+SWAY_DIR=~/.config/sway
+mkdir $SWAY_DIR
+ln -s "$configdir/sway/config" $SWAY_DIR/config
+ln -s "$configdir/sway/idle-$HOSTNAME" $SWAY_DIR/idle
+ln -s "$configdir/sway/input-$HOSTNAME" $SWAY_DIR/input
+make_sway_symlink init-brightnessbar.sh
+make_sway_symlink init-volumebar.sh
+make_sway_symlink status.sh
+make_sway_symlink bg-shuffle.sh
+
+
--- sway/status.sh
+++ sway/status.sh
@@ -12,19 +12,19 @@ loadAvgs() {
 }
 
 calcBattery() {
-    local bat_state=$(upower-i /org/freedesktop/UPower/devices/battery_BAT0 | rg 'state' | awk '{ print $2; }')
-    local percentage=$(upower-i /org/freedesktop/UPower/devices/battery_BAT0 | rg 'percentage' | awk '{ print $2; }')
+    local bat_state=$(upower -i /org/freedesktop/UPower/devices/battery_BAT0 | rg 'state' | awk '{ print $2; }')
+    local percentage=$(upower -i /org/freedesktop/UPower/devices/battery_BAT0 | rg 'percentage' | awk '{ print $2; }')
     if [[ ${bat_state} == charging ]]; then
         echo ${percentage}
     else
-        local timer=$(upower-i /org/freedesktop/UPower/devices/battery_BAT0 | rg 'time to empty' | awk '{ print $4 " " $5; }')
+        local timer=$(upower -i /org/freedesktop/UPower/devices/battery_BAT0 | rg 'time to empty' | awk '{ print $4 " " $5; }')
         echo "${timer-nan} (${percentage})"
     fi
 }
 
 volume=$(ponymix is-muted && { echo "--" } || { ponymix get-volume })
-battery=$(upower -i /org/freedesktop/UPower/devices/battery_BAT1 | rg percentage | awk '{ print $2; }')
-time_remaining=$(upower -i /org/freedesktop/UPower/devices/battery_BAT1 | rg "time to" | cut -d':' -f 2 | sed 's/^ *//')
+battery=$(upower -i /org/freedesktop/UPower/devices/battery_BAT0 | rg percentage | awk '{ print $2; }')
+time_remaining=$(upower -i /org/freedesktop/UPower/devices/battery_BAT0 | rg "time to" | cut -d':' -f 2 | sed 's/^ *//')
 stardate=$(stardate)
 timestamp=$(date +'%Y-%m-%d %H:%M')
 dayOfWeek=$(japaneseDayOfWeek)
