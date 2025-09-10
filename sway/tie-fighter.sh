649ad7904d2dc37076067cb98b75449892f43ef7
echoteecat <lcarsos@lcarsos.com>
2025-09-09 22:23:36 -0600

    some stored cleanups on mvb

--- sway/tie-fighter.sh
+++ sway/tie-fighter.sh
@@ -1,5 +1,13 @@
 #!/usr/bin/env bash
 
-sway "output DP-3 transform 270 position -1800 0"
-sway "output DP-2 transform 0 position 0 0"
-sway "output DP-1 transform 90 position 3840 0"
+if [[ $HOSTNAME == "jamesmonroe" ]]; then
+    sway "output DP-3 transform 270 position -1800 0"
+    sway "output DP-2 transform 0 position 0 0"
+    sway "output DP-1 transform 90 position 3840 0"
+elif [[ $HOSTNAME == "martinvanburen" ]]; then
+    sway "output eDP-1 position 0 0"
+    sway "output DP-6 transform 270 position 2560 0"
+    sway "output DP-5 transform 0 position 4000 0"
+    sway "output DP-1 transform 90 position 6560 0"
+
+fi
