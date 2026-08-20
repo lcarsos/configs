6516f81f02b7459f94084441575bc236228acb70
echoteecat <lcarsos@lcarsos.com>
2026-08-20 17:42:03 -0600

    added a new monitor

--- sway/tie-fighter.sh
+++ sway/tie-fighter.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
 if [[ $HOSTNAME == "jamesmonroe" ]]; then
-    sway "output DP-3 transform 270 position -1800 0"
-    sway "output DP-2 transform 0 position 0 0"
-    sway "output DP-1 transform 90 position 3840 0"
+    sway "output DP-3 transform 270 position 0 1440"
+    sway "output DP-2 transform 0 position 2160 1440"
+    sway "output DP-1 transform 90 position 6000 1440"
 elif [[ $HOSTNAME == "martinvanburen" ]]; then
     sway "output eDP-1 position 0 0"
     sway "output DP-6 transform 270 position 2560 0"
